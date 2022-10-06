b9f2d1269f09d2dd4008bab81b543c0564f112fa
IntoTheLines <110676026+IntoTheLines@users.noreply.github.com>
2022-10-06 23:43:51 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-giardino",
+      "name": "giardino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.130521954925566,
+          "pitch": 0.005691828046188618,
+          "rotation": 0,
+          "target": "7-opens"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-garage",
+      "name": "garage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0628924592173146,
+          "pitch": -0.006877162171671003,
+          "rotation": 0,
+          "target": "2-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corridoio",
+      "name": "corridoio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1825791367147964,
+          "pitch": -0.007226192971842238,
+          "rotation": 0,
+          "target": "4-camera"
+        },
+        {
+          "yaw": -0.7610903040256254,
+          "pitch": -0.006993249214319874,
+          "rotation": 0,
+          "target": "5-cameretta"
+        },
+        {
+          "yaw": 0.056227770425556045,
+          "pitch": 0.034630275105316244,
+          "rotation": 0,
+          "target": "3-bagno"
+        },
+        {
+          "yaw": 3.0974956909770697,
+          "pitch": 0.07796788161174284,
+          "rotation": 0,
+          "target": "1-garage"
+        },
+        {
+          "yaw": -2.7897430732017625,
+          "pitch": 0.09669084406754536,
+          "rotation": 0,
+          "target": "6-cameretta-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bagno",
+      "name": "bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25400472057064505,
+          "pitch": 0.028976468323012483,
+          "rotation": 0,
+          "target": "2-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-camera",
+      "name": "camera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5903557847198329,
+          "pitch": -0.01274944997673444,
+          "rotation": 0,
+          "target": "2-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cameretta",
+      "name": "cameretta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14546881453739324,
+          "pitch": 0.003117327966483785,
+          "rotation": 0,
+          "target": "2-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cameretta-2",
+      "name": "cameretta 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0696460040859819,
+          "pitch": -0.02702827970307453,
+          "rotation": 0,
+          "target": "2-corridoio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-opens",
+      "name": "opens",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4251961241593343
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.972705397743411,
+          "pitch": -0.00026612933890568513,
+          "rotation": 0,
+          "target": "2-corridoio"
+        },
+        {
+          "yaw": 3.025727925596181,
+          "pitch": -0.01125444444858914,
+          "rotation": 0,
+          "target": "1-garage"
+        },
+        {
+          "yaw": -1.7647123819521617,
+          "pitch": 0.02306896621498744,
+          "rotation": 0,
+          "target": "0-giardino"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
